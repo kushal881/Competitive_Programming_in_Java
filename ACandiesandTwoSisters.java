99419ccc3296a959e7510731575cefce498b9be4
kushal kamboj <kushal.23@lpu.in>
2025-09-22 22:03:47 +0530

    "Commited"

---
+++ ACandiesandTwoSisters.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ACandiesandTwoSisters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            if(n%2==0) System.out.println(n/2-1);
+            else if(n==1) System.out.println(0);
+            else System.out.println(n/2);
+        }
+    }
+}
---
+++ ATheNewYearMeetingFriends.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ATheNewYearMeetingFriends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(Math.max(Math.max(a,b),c) - Math.min(Math.min(a,b),c));
+    }
+}
