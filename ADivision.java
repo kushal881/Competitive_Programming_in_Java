b1ca451c69daa1f99b84b1be71ac363a8c7a1437
kushal kamboj <kushal.23@lpu.in>
2025-09-25 19:18:33 +0530

    "Commited"

---
+++ ADivision.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ADivision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int r = sc.nextInt();
+            if(r >= 1900) System.out.println("Division 1");
+            else if(r >= 1600) System.out.println("Division 2");
+            else if(r >= 1400) System.out.println("Division 3");
+            else System.out.println("Division 4");
+        }
+    }
+}
