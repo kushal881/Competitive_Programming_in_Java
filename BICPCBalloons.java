23cf3d0eebca47c32c63f36fd0cefa8d1ec938ee
kushal kamboj <kushal.23@lpu.in>
2025-10-04 21:56:27 +0530

    "Commited"

--- BICPCBalloons.java
+++ BICPCBalloons.java
@@ -13,6 +13,7 @@ public class BICPCBalloons {
             }
             for(int i=0;i<arr.length;i++){
                 if(arr[i]>0) {
+
                     sum += arr[i] + 1;
                 }
             }
