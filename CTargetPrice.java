2abeed6490bd58181e1b4b5e07c4c7689fd79666
kushal kamboj <kushal.23@lpu.in>
2025-10-09 16:21:45 +0530

    "Commited"

--- CTargetPrice.java
+++ CTargetPrice.java
@@ -1,26 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
+import java.lang.Math;
 
-public class CTargetPrice {
+public class CTargetPrice{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        sc.nextLine();
-
         while (t-- > 0) {
-            int totalScore = 0;
+            solve(sc);
+        }
+    }
 
-            for (int i = 0; i < 10; i++) {
-                String line = sc.nextLine();
-                for (int j = 0; j < 10; j++) {
-                    if (line.charAt(j) == 'X') {
-                        int distance = Math.max(Math.abs(i - 4), Math.abs(j - 4));
-                        int score = Math.max(0, 5 - distance);
-                        totalScore += score;
-                    }
+    private static void solve(Scanner sc) {
+        int totalScore = 0;
+        char[][] target = new char[10][10];
+        for (int i = 0; i < 10; i++) {
+            target[i] = sc.next().toCharArray();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (target[i][j] == 'X') {
+                    int ring;
+                    int rowDist = Math.min(i, 9 - i);
+                    int colDist = Math.min(j, 9 - j);
+                    ring = Math.min(rowDist, colDist);
+                    totalScore += (ring + 1);
                 }
             }
-
-            System.out.println(totalScore);
         }
+        System.out.println(totalScore);
     }
 }
