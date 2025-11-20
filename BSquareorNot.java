a838409230d0d10fff1c6b29d9b9bdeee167d10e
hk1 <kushal.23@lpu.in>
2025-11-20 14:10:35 +0530

    "Commited"

--- BSquareorNot.java
+++ BSquareorNot.java
@@ -1,54 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class BSquareorNot {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
-        int t = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
         while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            String s = br.readLine();
+            int n = sc.nextInt();
+            String s = sc.next();
 
             int r = (int) Math.sqrt(n);
 
             if (r * r != n) {
-                pw.println("No");
+                System.out.println("No");
                 continue;
             }
 
             boolean isPossible = true;
+
             for (int i = 0; i < r; i++) {
                 for (int j = 0; j < r; j++) {
-                    char characterInString = s.charAt(i * r + j);
-                    boolean isOnEdge = (i == 0 || i == r - 1 || j == 0 || j == r - 1);
+                    char ch = s.charAt(i * r + j);
+                    boolean isEdge = (i == 0 || i == r - 1 || j == 0 || j == r - 1);
 
-                    if (isOnEdge) {
-                        if (characterInString != '1') {
+                    if (isEdge) {
+                        if (ch != '1') {
                             isPossible = false;
                             break;
                         }
                     } else {
-                        if (characterInString != '0') {
+                        if (ch != '0') {
                             isPossible = false;
                             break;
                         }
                     }
                 }
-                if (!isPossible) {
-                    break;
-                }
+                if (!isPossible) break;
             }
 
-            if (isPossible) {
-                pw.println("Yes");
-            } else {
-                pw.println("No");
-            }
+            System.out.println(isPossible ? "Yes" : "No");
         }
-        pw.flush();
-        pw.close();
+
     }
 }
